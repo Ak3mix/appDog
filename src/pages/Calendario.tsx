0a56d1c5f6f2edadbe1743b1f0504a5dd627b8bb
Akemix~ <akemiwork2025>
2026-06-12 11:55:31 -0400

    Fix syntax error in Calendario.tsx

--- src/pages/Calendario.tsx
+++ src/pages/Calendario.tsx
@@ -27,28 +27,4 @@ const Calendario = () => {
   );
 };
 
-    loadEventos();
-  }, []);
-
-  return (
-    <Layout title="Calendario">
-      <Box sx={{ mb: 2 }}>
-        {['Todos', 'Vacunas', 'Citas', 'Tratamientos'].map(tipo => (
-          <Chip key={tipo} label={tipo} sx={{ mr: 1 }} />
-        ))}
-      </Box>
-      <Paper sx={{ p: 2 }}>
-        <Typography variant="h6" textAlign="center">Octubre 2023</Typography>
-        <Grid container spacing={1} sx={{ mt: 1 }}>
-            {Array.from({ length: 31 }).map((_, i) => (
-                <Grid item xs={1.7} key={i}>
-                    <Box sx={{ border: '1px solid #ccc', p: 1, textAlign: 'center' }}>
-                        {i + 1}
-                    </Box>
-                </Grid>
-            ))}
-        </Grid>
-      </Paper>
-    </Layout>
-  );
-};
+export default Calendario;
